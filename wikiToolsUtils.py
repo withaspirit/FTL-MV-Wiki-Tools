a3c9e99c1aa760780a4ae7e8361c946078191349
cyberphoria <61635007+cyberphoria@users.noreply.github.com>
2022-08-22 14:54:11 -0400

    Fix renaming errors

--- wikiToolsUtils.py
+++ wikiToolsUtils.py
@@ -2,27 +2,27 @@ import configparser
 import shutil
 import subprocess
 
-import init
+import wikiToolsInit
 
 # This file contains functions involving the slipstream's validate, patch,
 # and extract-dats commands. Also contains functions to zip and move a folder.
 
 config = configparser.ConfigParser()
-config.read(init.configFileName)
+config.read(wikiToolsInit.configFileName)
 
 # zipMove and patchExtract steps together
 def zipValidatePatchExtract(fileName: str, directory: str):
-    zipMove(config[init.projectPaths][init.project], fileName, directory)
+    zipMove(config[wikiToolsInit.projectPaths][wikiToolsInit.project], fileName, directory)
     zipName = f'{fileName}.zip'
     validate([zipName])
-    patch([init.multiverseName, zipName])
-    extractDats(config[init.zipPaths][init.ftl])
+    patch([wikiToolsInit.multiverseName, zipName])
+    extractDats(config[wikiToolsInit.zipPaths][wikiToolsInit.ftl])
 
 # Zip a mod folder and move it to SlipstreamModManager/mods
 # folder assumed to be in cwd
 def zipMove(originalPath: str, zipFolder: str, targetPath: str):
     zipPath = shutil.make_archive(f'{originalPath}{zipFolder}', 'zip', targetPath)
-    newPath = f'{config[init.mainPaths][init.slipstream]}/mods/{zipFolder}.zip'
+    newPath = f'{config[wikiToolsInit.mainPaths][wikiToolsInit.slipstream]}/mods/{zipFolder}.zip'
     shutil.move(zipPath, newPath)
 
 # NOTE: when there is bad syntax, subprocess.check_call will return non-zero exit status
@@ -41,8 +41,8 @@ def extractDats(filePath: str):
 
 # Execute command line argument with SlipstreamModManager
 def executeSlipstream(args: list[str]):
-    slipstreamPath = config[init.mainPaths][init.slipstream]
-    args = ['java', '-jar', init.modman] + args
+    slipstreamPath = config[wikiToolsInit.mainPaths][wikiToolsInit.slipstream]
+    args = ['java', '-jar', wikiToolsInit.modman] + args
     subprocess.check_call(args, cwd=slipstreamPath, shell=True)
 
 def executePythonFile(path: str, fileName: str):
